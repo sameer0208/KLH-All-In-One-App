1a5757919165698f9e7fd0e2eb4aebb24b342333
SAYYED SAMEER BASIR <josephferon0901@gmail.com>
2023-06-10 13:12:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+btn1 = document.getElementById("btnauth");
+btn1.addEventListener("click", function() {
+    document.getElementById("authorcard").style.display = "block";
+})
+btn2 = document.getElementById("clsbtn");
+btn2.addEventListener("click", function() {
+    document.getElementById("authorcard").style.display = "none";
+})
